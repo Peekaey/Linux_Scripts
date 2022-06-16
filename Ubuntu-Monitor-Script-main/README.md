7e094ac4597a77fa49ba687f7b2c87d4147c9ef5
Dallas <dallasjt31@gmail.com>
2022-06-16 20:50:13 +1000

    includes outdated monitor script

---
+++ Ubuntu-Monitor-Script-main/README.md
@@ -0,0 +1,3 @@
+Configuration file at startup for  monitors
+
+_Outdated as ubuntu has better display handling with the most recent versions - making this script outdated_
---
+++ Ubuntu-Monitor-Script-main/triple.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --mode 1920x1080 --pos 4480x0 --rotate left --output DP-0 --primary --mode 2560x1440 --rate 143  --pos 1920x225 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --mode 1920x1080 --rate 71 --pos 0x405 --rotate normal --output USB-C-0 --off --output DP-0  
