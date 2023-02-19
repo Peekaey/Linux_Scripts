41bb1896c9baee840286b48efde6649651485158
Dallas <dallasjt31@gmail.com>
2023-02-19 22:13:56 +1100

    updated again

--- DockerTeamCityServerInstall/TCSInstall.sh
+++ DockerTeamCityServerInstall/TCSInstall.sh
@@ -21,6 +21,8 @@ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev
 sudo apt-get update
 echo installing docker
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
 #Installing Teamcity server docker instance
 echo installing teamcity
@@ -29,21 +31,9 @@ mkdir teamcity/data
 mkdir teamcity/logs
 mkdir teamcity/agent
 sudo docker pull jetbrains/teamcity-server
-sudo docker run -it -d 
- --name server \
- -u  root \
- -v /teamcity/data:/data/teamcity_server/datadir \
- -v /teamcity/logs:/opt/teamcity/logs \
- -p 8111:8111 \
- jetbrains/teamcity-server
+sudo docker run -it -d --name server -u  root -v /teamcity/data:/data/teamcity_server/datadir -v /teamcity/logs:/opt/teamcity/logs -p 8111:8111 jetbrains/teamcity-server
 
 # Installing MariaDB docker instance
 echo installing MariaDB
- sudo docker run --detach \
-  -p 3306:3306 \
-  --name some-mariadb \
-  --env MARIADB_USER=$username \
-  --env MARIADB_PASSWORD=$user_password \
-  --env MARIADB_ROOT_PASSWORD=$root_passwod \
-  mariadb:latest
+sudo docker run --detach -p 3306:3306 --name some-mariadb --env MARIADB_USER=$username --env MARIADB_PASSWORD=$user_password --env MARIADB_ROOT_PASSWORD=$root_passwod mariadb:latest
 
